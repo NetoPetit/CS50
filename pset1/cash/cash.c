a98102c8be8d65c58a182ffad347e6b71e106429
NetoPetit <cidao_petit5@hotmail.com>
2024-02-07 01:16:01 -0300

    cash

--- pset1/cash/cash.c
+++ pset1/cash/cash.c
@@ -1,13 +1,32 @@
 #include <stdio.h>
+#include <math.h>
 #include <cs50.h>
 
 int main(void)
 {
-    float entrada;
+    float troco;
+    int centavos, indice;
+    int quantidade = 0;
+
+    int moedas[] = {25, 10, 5, 1};
+    
     do
     {
-        entrada = get_float("Entrada: ");
-    } while (entrada < 1);
-    
-}
+        troco = get_float("Troco desejado: ");
+    } while (troco <= 0);
 
+    centavos = round(troco * 100);
+
+    for(indice = 0; indice < 5; indice ++)
+    {
+        while (centavos - moedas[indice] >= 0 && centavos > 0)
+        {
+            centavos = centavos - moedas[indice];
+            quantidade ++;
+        }
+        
+    }
+
+    printf("%i\n", quantidade);
+
+}
